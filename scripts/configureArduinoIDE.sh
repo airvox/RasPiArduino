783e3ddfeae12569daf3f3cf304bdcd881675cba
yasir1brahim <yasir.ibrahim@gmail.com>
2016-08-01 12:36:35 +0500

    Update configureArduinoIDE.sh
    command to add tool chains , has been removed .
    because tool chains have been added to the repository

--- scripts/configureArduinoIDE.sh
+++ scripts/configureArduinoIDE.sh
@@ -1,5 +1,3 @@
 mkdir -p hardware/RaspberryPi/
 cd hardware/RaspberryPi/
-git init
 git clone https://github.com/yasir1brahim/RasPiArduino.git piduino
-wget -qO- https://github.com/me-no-dev/RasPiArduino/releases/download/0.0.1/arm-linux-gnueabihf-linux64.tar.gz | tar xvz -C piduino/tools
