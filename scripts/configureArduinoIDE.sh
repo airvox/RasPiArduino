14b6a3a604abb2dcf3f52a17f90d24e6439bf45d
yasir1brahim <yasir.ibrahim@gmail.com>
2016-08-01 14:51:36 +0500

    Update configureArduinoIDE.sh

--- scripts/configureArduinoIDE.sh
+++ scripts/configureArduinoIDE.sh
@@ -1,3 +1,17 @@
-mkdir -p hardware/RaspberryPi/
-cd hardware/RaspberryPi/
-git clone https://github.com/yasir1brahim/RasPiArduino.git piduino
+#!/bin/bash
+
+LASTFOLDER=${PWD##*/}
+REQUIREDFOLDER="arduino-1.6.9"
+
+if [ $LASTFOLDER == $REQUIREDFOLDER ]; then
+
+    mkdir -p hardware/RaspberryPi/
+    cd hardware/RaspberryPi/
+    git clone https://github.com/yasir1brahim/RasPiArduino.git piduino
+
+else
+	echo Place the configureArduinoIDE.sh file in the Arduino-1.6.9 folder
+fi
+
+
+
